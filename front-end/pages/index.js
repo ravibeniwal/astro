31245b7ce0f04099ba1d1f97a4c12d0fee17411e
Ravi Beniwal <ravibeniwal35@gmail.com>
2021-08-11 20:03:10 +0530

    Updated the package.json file

--- front-end/pages/index.js
+++ front-end/pages/index.js
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import Image from "next/image";
+// import Image from "next/image";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
@@ -60,9 +60,9 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           Powered by{" "}
-          <span className={styles.logo}>
+          {/* <span className={styles.logo}>
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
+          </span> */}
         </a>
       </footer>
     </div>
